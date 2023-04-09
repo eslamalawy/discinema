0e38dd61b68c29a18d291b556997eb4d837e3fae
Eslam Alawy <eslammo4kala@gmail.com>
2023-04-09 07:04:18 +0200

    inistalling react router dom and tailwend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+       <NavBar />
+      <h1 className="text-7xl mr-0 text-green-700">Hello TailWend CSS</h1>
+    </div>
+  );
+}
+
+export default App;
